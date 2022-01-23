bdc92dca6b078471fb503e4f3fdc18ad68fff765
kartogishenko <kartogishenko@gmail.com>
2022-01-23 13:45:31 +0100

    Ny bussapp

---
+++ js/script.js
@@ -0,0 +1,112 @@
+// Skapar kartan och centrerar på Sjöbo
+//const map = L.map('map').setView([55.6113, 13.6890], 13);
+const map = L.map('map').fitWorld();
+
+
+// OpenStreetMap bakgrundskarta
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+  }).addTo(map);
+
+// Busshållplatser som oranga pluttar
+let busIcon = L.icon({
+    iconUrl: 'css/images/dot32.png',
+    iconSize: [32, 32],
+});
+
+
+// Editerbart lager från ArcGIS Online
+const busBus = L.esri.featureLayer({
+    url: 'https://services5.arcgis.com/yWkBPqDRh3zjV90Q/arcgis/rest/services/BussHpl/FeatureServer/0',
+    pointToLayer: function (geojson, latlng) {
+        return L.marker(latlng, {
+          icon: busIcon
+        });
+      }
+}).addTo(map);
+
+// Start/Stop tracking av position
+function toggle_tracker(){
+  currentvalue = document.getElementById('onoff').value;
+  if(currentvalue == "Track"){
+    get_location();
+    document.getElementById("onoff").value="No track";
+  }else{
+    document.getElementById("onoff").value="Track";
+    navigator.geolocation.clearWatch(id);
+  }
+};
+
+// Geolocation, zoomar till position och visar en ikon
+function get_location() {
+  if (Modernizr.geolocation) {
+      id = navigator.geolocation.watchPosition(show_map, handle_error, {
+        enableHighAccuracy: true,
+        timeout: 3000,
+        maximumAge: 30000,
+      });
+  } else {
+      // no native support
+  }
+};
+function show_map(position) {
+  latitude = position.coords.latitude;
+  longitude = position.coords.longitude;
+  L.marker([latitude, longitude]).addTo(map);
+  map.setView([latitude, longitude], 13);
+};
+function handle_error(error) {
+    // Display error based on the error code.
+    const { code } = error;
+    switch (code) {
+      case GeolocationPositionError.TIMEOUT:
+        // Handle timeout.
+        break;
+      case GeolocationPositionError.PERMISSION_DENIED:
+        // User denied the request.
+        break;
+      case GeolocationPositionError.POSITION_UNAVAILABLE:
+        // Position not available.
+        break;
+    }
+};
+
+// Loggar position
+function log_position() {
+  let feat = L.marker([latitude, longitude]).toGeoJSON();
+  busBus.addFeature(feat, function (err, response) {
+      if (err) {
+          return;
+      }
+
+      console.log(response);
+  });
+};
+
+// Lägg till objekt om man klickar på kartan
+map.on('click', function (e) {
+  let feat = L.marker(e.latlng).toGeoJSON();
+  busBus.addFeature(feat, function (err, response) {
+      if (err) {
+          return;
+      }
+
+      console.log(response);
+  });
+});
+
+
+// Ta bort kartobjekt om de klickas på
+busBus.on('click', function (e) {
+  busBus.deleteFeature(e.layer.feature.id, function (err, response) {
+    if (err) {
+      return;
+    }
+
+    console.log(response);
+  });
+
+// make sure map click event isn't fired.
+L.DomEvent.stopPropagation(e);
+});
